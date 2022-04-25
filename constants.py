046953ef7bbfc324174b71a6eff942d114947b2a
razin <r42intailor@gmail.com>
2022-04-25 05:43:39 +0530

    removed files

--- constants.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-if sys.version_info < (3, 8):  # pragma: no cover (<PY38)
-    import importlib_metadata
-else:  # pragma: no cover (PY38+)
-    import importlib.metadata as importlib_metadata
-
-VERSION = '0.0.1'
